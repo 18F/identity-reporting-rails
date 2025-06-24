e636bdfebdf96c3be0644a45b884a8168753069b
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-06-24 14:39:11 -0500

    update for redshift

--- db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
+++ db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
@@ -1,7 +1,25 @@
 class UpdateLogsProductionTablecolumn < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
   
-  def change
-    change_column 'logs.production', :duration, :decimal, precision: 10, scale: 6
+  def up
+    if connection.adapter_name.downcase.include?('redshift')
+      execute "ALTER TABLE logs.production RENAME COLUMN duration TO duration_old"
+      execute "ALTER TABLE logs.production ADD COLUMN duration DECIMAL(10,6)"
+      execute "UPDATE logs.production SET duration = duration_old::DECIMAL(10,6)"
+      execute "ALTER TABLE logs.production DROP COLUMN duration_old"
+    else
+      change_column 'logs.production', :duration, :decimal, precision: 10, scale: 6
+    end
+  end
+  
+  def down
+    if connection.adapter_name.downcase.include?('redshift')
+      execute "ALTER TABLE logs.production RENAME COLUMN duration TO duration_old"      
+      execute "ALTER TABLE logs.production ADD COLUMN duration FLOAT"
+      execute "UPDATE logs.production SET duration = duration_old::FLOAT"
+      execute "ALTER TABLE logs.production DROP COLUMN duration_old"
+    else
+      change_column 'logs.production', :duration, :float
+    end
   end
 end
