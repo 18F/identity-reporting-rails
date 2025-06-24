e747a6f8b51ae8789646c8e60df5565de646843e
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-06-24 14:17:41 -0500

    disable transaction block

--- db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
+++ db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
@@ -1,4 +1,6 @@
 class UpdateLogsProductionTablecolumn < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+  
   def change
     change_column 'logs.production', :duration, :decimal, precision: 10, scale: 6
   end
