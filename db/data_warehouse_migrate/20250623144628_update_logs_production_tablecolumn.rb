02f029ed771588891b60d2fe219cf4eba10ac3ed
aaron.nagucki <aaron.nagucki@gsa.gov>
2025-06-30 19:37:38 +0000

    1051 duration precision issue

---
+++ db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
@@ -0,0 +1,25 @@
+class UpdateLogsProductionTablecolumn < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+  
+  def up
+    if connection.adapter_name.downcase.include?('redshift')
+      execute "ALTER TABLE logs.production RENAME COLUMN duration TO duration_old"
+      execute "ALTER TABLE logs.production ADD COLUMN duration DECIMAL(10,6) NULL"
+      execute "UPDATE logs.production SET duration = duration_old::DECIMAL(10,6)"
+      execute "ALTER TABLE logs.production DROP COLUMN duration_old"
+    else
+      change_column 'logs.production', :duration, :decimal, precision: 10, scale: 6, null: true
+    end
+  end
+  
+  def down
+    if connection.adapter_name.downcase.include?('redshift')
+      execute "ALTER TABLE logs.production RENAME COLUMN duration TO duration_old"      
+      execute "ALTER TABLE logs.production ADD COLUMN duration FLOAT NULL"
+      execute "UPDATE logs.production SET duration = duration_old::FLOAT"
+      execute "ALTER TABLE logs.production DROP COLUMN duration_old"
+    else
+      change_column 'logs.production', :duration, :float, null: true
+    end
+  end
+end
