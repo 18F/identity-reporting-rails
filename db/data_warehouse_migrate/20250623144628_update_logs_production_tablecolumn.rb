106b507301c7db1c4a75ace237c89e09d1c699bf
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-07-14 11:47:40 -0500

    update migration content

--- db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
+++ db/data_warehouse_migrate/20250623144628_update_logs_production_tablecolumn.rb
@@ -4,11 +4,11 @@ class UpdateLogsProductionTablecolumn < ActiveRecord::Migration[7.2]
   def up
     if connection.adapter_name.downcase.include?('redshift')
       execute "ALTER TABLE logs.production RENAME COLUMN duration TO duration_old"
-      execute "ALTER TABLE logs.production ADD COLUMN duration DECIMAL(10,6) NULL"
-      execute "UPDATE logs.production SET duration = duration_old::DECIMAL(10,6)"
+      execute "ALTER TABLE logs.production ADD COLUMN duration DECIMAL(15,4) NULL"
+      execute "UPDATE logs.production SET duration = duration_old::DECIMAL(15,4)"
       execute "ALTER TABLE logs.production DROP COLUMN duration_old"
     else
-      change_column 'logs.production', :duration, :decimal, precision: 10, scale: 6, null: true
+      change_column 'logs.production', :duration, :decimal, precision: 15, scale: 4, null: true
     end
   end
   
