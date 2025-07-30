fb9b937a9de568b97f822047b9acc697f9cf87cd
Henry Richardson <henry.richardson@gsa.gov>
2025-07-30 17:49:13 -0400

    feat: add development environment setup with devenv
    - Introduced `.envrc` for direnv integration to manage environment variables and setup.
    - Added `.gitignore` entries for devenv, direnv, and pre-commit-related files.
    - Created `.gitlab-ci-devenv.yml` for CI integration with devenv, including caching and cleanup logic.
    - Added `.gitlab-ci-rules.yml` to define reusable CI rules.
    - Updated `.gitlab-ci.yml` to include the new devenv-related CI configurations.
    - Added `devenv.lock` to lock dependencies for reproducibility.
    - Created `devenv.nix` to define the development environment, including Ruby setup, packages, and PostgreSQL service.
    - Added `devenv.yaml` to configure inputs for the devenv setup.

---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/packages/
+
+  languages = {
+    ruby = {
+      enable = true;
+      bundler.enable = true;
+      versionFile = ./.ruby-version;
+    };
+  };
+
+  packages = with pkgs; [
+    foreman
+    git
+    glab
+    gnumake
+    libyaml
+  ];
+
+  tasks = {
+    "ruby:install_gems" = {
+      exec = "bundle install";
+      status = "bundle check";
+      before = [ "devenv:enterShell" ];
+    };
+  };
+
+  enterShell = ''
+    # Conflicts with bundler
+    export RUBYLIB=
+  '';
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    listen_addresses = "127.0.0.1";
+  };
+}
