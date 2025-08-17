202f2493700583698ba5e7ef7a22922b78f72f32
Birane Seck <birane.seck@gsa.gov>
2025-08-17 19:58:00 -0400

    Create decryption udf function through rails migration

---
+++ db/data_warehouse_migrate/20250815200739_create_redshift_descryption_udf.rb
@@ -0,0 +1,26 @@
+class CreateRedshiftDescryptionUdf < ActiveRecord::Migration[7.2]
+  def change
+    if connection.adapter_name.downcase.include?('redshift')
+      reversible do |dir|
+        env_name = Identity::Hostdata.env
+        account_id = Identity::Hostdata.aws_account_id
+        lambda_name = "#{env_name}-redshift-idp-decryption-udf"
+        redshift_iam_role_name = "arn:aws:iam::#{account_id}:role/#{env_name}-redshift-iam-role"
+        dir.up do
+          execute <<-SQL
+          CREATE OR REPLACE EXTERNAL FUNCTION decrypt_udf (encrypted_value varchar)
+          RETURNS varchar STABLE
+          LAMBDA '#{lambda_name}'
+          IAM_ROLE '#{redshift_iam_role_name}';
+          SQL
+        end
+
+        dir.down do
+          execute <<-SQL
+          DROP FUNCTION decrypt_udf(varchar);
+          SQL
+        end
+      end
+    end
+  end
+end
