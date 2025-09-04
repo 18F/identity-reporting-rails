7e79b14248fe84909c490d36ca5a9e9307921b2a
samathad2023 <sdondeti@flexion.us>
2025-09-04 14:52:02 -0700

    mock key

--- app/jobs/attempts_api_import_job.rb
+++ app/jobs/attempts_api_import_job.rb
@@ -2,37 +2,40 @@ require 'net/http'
 require 'json'
 require 'jwt'
 
+PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuUMLV31VkBf+UkFG/gyL\nPbjUpsGYF6OoXUu9IsF3/fUNMdELO0yTQP/79iPHr0HlTcDVK3NhMostHY0FK7b7\nrg/SeSLoxIBLQth+NuYK8b8VVyOZv3S4TU0XJQu5ihi0LlyjOsNNmtnEjhK7G56N\nNIfPW65eghhF2rMNhHJqDgTMYPj2jwyx1Iz/t25BlL5mexVPixom8Suht22np7FQ\nJUlOsdesTww8yRlik8uODkb1QCuBFR7pYAErtL9RE3fsj28QRxRr1+bXeQa3FtHf\nY/8rYuTJVkL630hvUBXDTEv26FhkG+cI3jQJ0HLiYfacO5v4+btaCkIp2z4LjL8X\n6wIDAQAB\n-----END PUBLIC KEY-----\n"
+PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAuUMLV31VkBf+UkFG/gyLPbjUpsGYF6OoXUu9IsF3/fUNMdEL\nO0yTQP/79iPHr0HlTcDVK3NhMostHY0FK7b7rg/SeSLoxIBLQth+NuYK8b8VVyOZ\nv3S4TU0XJQu5ihi0LlyjOsNNmtnEjhK7G56NNIfPW65eghhF2rMNhHJqDgTMYPj2\njwyx1Iz/t25BlL5mexVPixom8Suht22np7FQJUlOsdesTww8yRlik8uODkb1QCuB\nFR7pYAErtL9RE3fsj28QRxRr1+bXeQa3FtHfY/8rYuTJVkL630hvUBXDTEv26Fhk\nG+cI3jQJ0HLiYfacO5v4+btaCkIp2z4LjL8X6wIDAQABAoIBADO9TKqXf1Dp0oYo\nYupKmIyZVRJpWM+pTjkYEO9m9tr/GDtEdY2PGkT5+OVKIteIYMDxTNeAtrGF+wiY\nvMvzo6B+HLrmC/ntFpfJnJ46au4O/qfCcorszxgPopofBydRNOkJyDB+IOtRTDqd\ncpIJGsiD8V5aeVx96OxmOy01Qvc812uEHbcGwiKT1KMiStvoc40t86mvXZFy+pxi\nALZbS9uCaacr3zIAqHe/yTUkiuiVENhtEwbm9oO7yLrpvYar/h+WTIe7zaWRcy1e\nwei8CNG3kLwInMpcB6zYT5mDAiny0xarP/V82sg+OWL4xm1hh0LN+ptV0C5neFYr\n5KBtBLkCgYEA85T59aw5Yyal7iwjeBK14fKfePkWSodN+FcYUGz4HfTaMcQVmyOg\nkgllDSobDdvfCiMLq2p4Z1LAr9jw2LNGaVhoYMEbhpfrN3qG8vgj9RT/5gubeWL1\npQytuuGmppptLwD5Ru8u0j0twaNd/PuR30DTlJ4V5FrD7+/Cvos6HPcCgYEAwrTs\npkqrrjfh9dnNCDjLTh2bt3Dds5a2vE8w484zK/fBfiuvJt1nhTPgA9Q3XE4ZoOEJ\nzJNVqJqDoLtkgBAOT4ooWaTg3GQflqrfgG5SI+jcPd0X0Mhy+SJQrjlGmNBnTifu\nBacQhaq7NMJ8ctiYnsN2MzCoubM/FF7QOzlqY60CgYBTqM41F8LaEBMbe2NvQRXh\nFcC5/usuC8y2x8sdDGAngcpTH2LAVvs2TS282MJT/zlatPC0HixeaGivvNXzx5ce\nZXPsD0cR8imic13YI2vOKDk/3Kq4hUmTN4iP0CK5w/5OD4qOV2YAZWzKvf5w/kJ6\nqDxDJgyk0pvLU9DSOuVpvwKBgQCgFhms8CAP8ip69US6ydd+tqFdRhNCoVxFn+bW\npqc0M1SH5GryTX/b6Tb6bvXFkwFHYT+pUEpRghlPgkOzd4AMrc9XRVUMX9YJDx0M\nf6hRlhffVXVLWEQPysMDPFxrMI5/mBz/0Mio3iNl0bJ4ytVRU+xPUmanqFTo7rvO\ntfpfUQKBgFa2toGR4ByxUjfkvSKDOCpGuiJHqjs+7LPntYvEtN6+3ffTyhaw1uRR\n8XJ/ekWsmBSYX72CZofqSpsLaBEIeQ1bKaH96mmd7CLXvyPDFVs7jLPfg8xo+EzZ\nMltml2puTb9hCbsBmbtRnOhI0UD06YkcrYHEtjvTA1dbqJmH6mMz\n-----END RSA PRIVATE KEY-----\n"
+
 class AttemptsApiImportJob < ApplicationJob
   queue_as :default
 
-  API_URL = 'https://localhost:3000/attemptsapi'
+  # API_URL = 'https://localhost:3000/attemptsapi'
   # HEADERS = { 'Content-Type' => 'application/json' }
   TABLE_NAME = 'fcms.unextracted_events'
 
   def perform
     log_info('AttemptsApiImportJob: Job started', true)
     loop do
-      begin
-        key_pair = create_key_pair
-        encrypted_jwt_payload = encrypt_jwt(mock_jwt_payload, key_pair[:public_key])
-        import_to_redshift(encrypted_jwt_payload)
+      # begin
+      public_key = IdentityConfig::Hostdata.config.api_public_key
+      encrypted_jwt_payload = encrypt_jwt(mock_jwt_payload, public_key)
+      import_to_redshift(encrypted_jwt_payload)
 
-        # Call the API request service
-        # Attempts::ApiRequestService.new.call
-        # mock api call
-        # sleep(1)
-        # mock api response
-        # response = Net::HTTP.get_response(URI(API_URL))
-        # if response.is_a?(Net::HTTPSuccess)
-        #   events = response.body
-        #   log_info('AttemptsApiImportJob: API call succeeded', true, events)
-        #   import_to_redshift(events)
-        # else
-        #   log_info('AttemptsApiImportJob: API call failed', false, response.body)
-        # end
-      rescue => e
-        log_info('AttemptsApiImportJob: Error during API attempt', false, { error: e.message })
-      end
+      # Call the API request service
+      # Attempts::ApiRequestService.new.call
+      # mock api call
+      # sleep(1)
+      # mock api response
+      # response = Net::HTTP.get_response(URI(API_URL))
+      # if response.is_a?(Net::HTTPSuccess)
+      #   events = response.body
+      #   log_info('AttemptsApiImportJob: API call succeeded', true, events)
+      #   import_to_redshift(events)
+      # else
+      #   log_info('AttemptsApiImportJob: API call failed', false, response.body)
+      # end
+      # rescue => e
+      #   log_info('AttemptsApiImportJob: Error during API attempt', false, { error: e.message })
+      # end
       sleep 5
     end
     log_info('AttemptsApiImportJob: Job completed', true)
