94470b2d2ed2e9b74ee57242d9a8cf543a1c6db8
samathad2023 <sdondeti@flexion.us>
2025-09-08 14:20:07 -0700

    pii decrypt job

--- app/jobs/fcms_pii_decrypt_job.rb
+++ app/jobs/fcms_pii_decrypt_job.rb
@@ -2,43 +2,98 @@ class FcmsPiiDecryptJob < ApplicationJob
   queue_as :default
 
   def perform
-    # fetch_data_from_redshift
+    fetch_insert_delete_data_from_redshift
+    insert_data_to_redshift_events
+    log_info('FcmsPiiDecryptJob: Job completed', true)
+  rescue => e
+    log_info('FcmsPiiDecryptJob: Job failed', false, { error: e.message })
+    raise e
   end
 
   private
 
-  # def fetch_data_from_redshift
-  #   build_params = {
-  #     line_count_threshold: Identity::Hostdata.config.unload_line_count_threshold,
-  #     five_minutes_ago: 5.minutes.ago.utc.strftime('%Y-%m-%d %H:%M'),
-  #   }
-  #   query = format(<<~SQL, build_params)
-  #     SELECT userid, start_time, end_time, line_count, path as s3_path, query as query_id
-  #     FROM stl_unload_log
-  #     WHERE line_count > %{line_count_threshold}
-  #       AND start_time > '%{five_minutes_ago}'
-  #   SQL
+  # fetch_data_from_redshift
+  # insert_data_to_redshift(encrypted_events)
+  # delete_data_from_redshift_unextracted_events 
+  # insert_data_to_redshift_events(with jti as key and decrypted message as message)
+  # update encrypted_events processed_timestamp
 
-  #   result = DataWarehouseApplicationRecord.connection.exec_query(query).to_a
-  #   if result.present?
-  #     merge_result = result.map do |row|
-  #       {
-  #         userid: row['userid'],
-  #         starttime: row['start_time'].strftime('%Y-%m-%d %H:%M'),
-  #         endtime: row['end_time'].strftime('%Y-%m-%d %H:%M'),
-  #         s3_path: row['s3_path'],
-  #         query_id: row['query_id'],
-  #         line_count: row['line_count'],
-  #       }
-  #     end
-  #     log_info(
-  #       "RedshiftUnloadLogCheckerJob: Found #{result.count} unload logs above threshold", false,
-  #       data: merge_result
-  #     )
-  #   else
-  #     log_info('RedshiftUnloadLogCheckerJob: No unload logs found above threshold', true)
-  #   end
-  # end
+  def decrypt_jwt(encrypted_jwt, public_key)
+    decrypted_jwt = JWE.decrypt(encrypted_jwt, public_key)
+    JSON.parse(decrypted_jwt)
+  end
+
+  def fetch_insert_delete_data_from_redshift
+    query = <<-SQL
+      WITH moved_records AS (
+        DELETE FROM fcms.unextracted_events
+        RETURNING message
+      )
+      INSERT INTO fcms.encrypted_events (message, import_timestamp)
+      SELECT message, CURRENT_TIMESTAMP FROM moved_records;
+    SQL
+    DataWarehouseApplicationRecord.connection.exec_query(query).to_a
+    log_info('FcmsPiiDecryptJob: Data fetch from unextracted_events to encrypted_events succeeded', true)
+
+  rescue ActiveRecord::StatementInvalid => e
+    log_info('FcmsPiiDecryptJob: Data fetch from unextracted_events to encrypted_events failed', false, { error: e.message })
+    raise e
+  end
+
+  def insert_data_to_redshift_events
+    encrypted_events = DataWarehouseApplicationRecord.connection.exec_query(
+      'SELECT message FROM fcms.encrypted_events WHERE processed_timestamp IS NULL'
+    ).to_a
+
+    return if encrypted_events.empty?
+
+    public_key = JobHelpers::AttemptsApiKeypairHelper.public_key
+    # import_timestamp = Time.current
+
+    values = encrypted_events.map do |event|
+      message = decrypt_jwt(event['message'], public_key)
+      jti = message['jti']
+      sanitized_message = ActiveRecord::Base.connection.quote(message.to_json)
+      sanitized_jti = ActiveRecord::Base.connection.quote(jti)
+      "(#{sanitized_jti}, #{sanitized_message}, CURRENT_TIMESTAMP)"
+    end.join(", ")
+
+    if values.empty?
+      log_info('FcmsPiiDecryptJob: No new encrypted events to process', true)
+      return
+    end
+
+    insert_query = <<-SQL.squish
+      INSERT INTO fcms.events (jti, message, import_timestamp)
+      VALUES #{values}
+      ON CONFLICT (jti) DO NOTHING;
+    SQL
+
+    begin
+      DataWarehouseApplicationRecord.connection.execute(insert_query)
+      log_info(
+        'FcmsPiiDecryptJob: Data insert to Redshift events succeeded', true,
+        { row_count: encrypted_events.size }
+      )
+    rescue ActiveRecord::StatementInvalid => e
+      log_info('FcmsPiiDecryptJob: Data insert to Redshift events failed', false, { error: e.message })
+      raise e
+    end
+    update_encrypted_events_processed
+  end
+
+  def update_encrypted_events_processed
+    begin
+      DataWarehouseApplicationRecord.connection.execute(
+        'UPDATE fcms.encrypted_events SET processed_timestamp = CURRENT_TIMESTAMP WHERE processed_timestamp IS NULL'
+      )
+      log_info('FcmsPiiDecryptJob: Updated processed_timestamp in encrypted_events', true)
+    rescue ActiveRecord::StatementInvalid => e
+      log_info('FcmsPiiDecryptJob: Failed to update processed_timestamp in encrypted_events', false, { error: e.message })
+      raise e
+    end
+  end
+
 
   def log_info(message, success, additional_info = {})
     Rails.logger.info(
