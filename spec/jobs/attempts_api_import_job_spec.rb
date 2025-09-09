f2c76a343a62bae76257d9ab9fda6d4a252b4ed3
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2025-09-09 15:23:44 -0700

    Fixed initial tests

--- spec/jobs/attempts_api_import_job_spec.rb
+++ spec/jobs/attempts_api_import_job_spec.rb
@@ -32,37 +32,26 @@ RSpec.describe AttemptsApiImportJob, type: :job do
         expect(Rails.logger).to receive(:info).with('{"job":"AttemptsApiImportJob","success":true,"message":"AttemptsApiImportJob: Job completed"}')
         fcms_job.perform
 
-        # result = DataWarehouseApplicationRecord.connection.execute(
-        #   "SELECT * FROM fcms.unextracted_events WHERE key_hash = 'event-1234-0'",
-        # ).to_a
-        # expect(result.length).to eq(1)
-        # expect(result.first['key_hash']).to eq('event-1234-0')
-        # expect(result.first['message']).to start_with('eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJ0eXAiOiJzZWNldmVudCtqd2UiLCJ6aXAiOiJERUYiLCJraWQiOiIxYmZiODYxMmE2NWU3NTkzOThlNGM0NGZmOWRkMWQ4OTZlNzBk')
+        result = DataWarehouseApplicationRecord.connection.execute(
+          "SELECT * FROM fcms.unextracted_events WHERE key_hash = 'event-1234-0'",
+        ).to_a
+        expect(result.length).to eq(1)
+        expect(result.first['key_hash']).to eq('event-1234-0')
+        expect(result.first['message']).to start_with('eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJ0eXAiOiJzZWNldmVudCtqd2UiLCJ6aXAiOiJERUYiLCJraWQiOiIxYmZiODYxMmE2NWU3NTkzOThlNGM0NGZmOWRkMWQ4OTZlNzBk')
       end
 
-      # it 'handles exceptions during data import' do
-      #   allow(DataWarehouseApplicationRecord.connection).to receive(:execute).and_raise(ActiveRecord::StatementInvalid.new('DB error'))
-      #   expect(Rails.logger).to receive(:info).with('AttemptsApiImportJob: Job started')
-      #   expect(Rails.logger).to receive(:info).with(
-      #     {
-      #       job: 'AttemptsApiImportJob',
-      #       success: false,
-      #       message: 'AttemptsApiImportJob: Data import to Redshift failed',
-      #       error: 'DB error',
-      #     },
-      #   )
-      #   allow(DataWarehouseApplicationRecord.connection).to receive(:execute).and_raise(ActiveRecord::StatementInvalid.new('DB error'))
-      #   expect(Rails.logger).to receive(:info).with('AttemptsApiImportJob: Job started')
-      #   expect(Rails.logger).to receive(:info).with(
-      #     {
-      #       job: 'AttemptsApiImportJob',
-      #       success: false,
-      #       message: 'AttemptsApiImportJob: Data import to Redshift failed',
-      #       error: 'DB error',
-      #     },
-      #   )
-      #   expect { fcms_job.perform }.to raise_error(ActiveRecord::StatementInvalid, 'DB error')
-      # end
+      it 'handles exceptions during data import' do
+        allow(DataWarehouseApplicationRecord.connection).to receive(:execute).
+          and_raise(ActiveRecord::StatementInvalid.new('DB error'))
+
+        allow(Rails.logger).to receive(:info)
+
+        expect { fcms_job.perform }.to raise_error(ActiveRecord::StatementInvalid, 'DB error')
+
+        expect(Rails.logger).to have_received(:info).with(
+          a_string_including('"success":false', 'Data import to Redshift failed', 'DB error'),
+        )
+      end
     end
   end
 end
