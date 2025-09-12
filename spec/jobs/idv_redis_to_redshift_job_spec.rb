e360de0a3098527fa4c4f496cf3c46266e6f4a19
Birane Seck <birane.seck@gsa.gov>
2025-09-12 17:03:16 -0400

    redis to redshift spec

--- spec/jobs/idv_redis_to_redshift_job_spec.rb
+++ spec/jobs/idv_redis_to_redshift_job_spec.rb
@@ -3,21 +3,21 @@ require 'factory_bot'
 
 RSpec.describe IDVRedisToRedshiftJob, type: :job do
   let(:fcms_job) { IDVRedisToRedshiftJob.new }
+  let(:redis_client) { AttemptsApi::RedisClient.new }
 
   describe '#perform' do
-    context 'when mock api returns an encrypted event' do
+    context 'when idv records are present in redis' do
       before do
-        allow(fcms_job).to receive(:fetch_api_data).and_return(
-          {
-            sets: [
-              { 'event-1234-0' => 'eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJ0eXAiOiJzZWNldmVudCtqd2UiLCJ6aXAiOiJERUYiLCJraWQiOiIxYmZiODYxMmE2NWU3NTkzOThlNGM0NGZmOWRkMWQ4OTZlNzBk' },
-            ],
-          }.with_indifferent_access,
-        )
-        # allow(FcmsPiiDecryptJob).to receive(:perform_now)
+        # write multiple events to redis
+        5.times do |i|
+          redis_client.write_event(
+            event_key: "event-1234-#{i}",
+            jwe: "eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00iLCJ0eXAiOiJzZWNldmVudCtqd2UiLCJ6aXAiOiJERUYiLCJraWQiOiIxYmZiODYxMmE2NWU3NTkzOThlNGM0NGZmOWRkMWQ4OTZlNzBk",
+            timestamp: Time.current,
+          )
       end
 
-      it 'imports the events into fcms.unextracted_events and triggers decryption job' do
+      it 'imports the events into fcms.unextracted_events' do
         allow(Rails.logger).to receive(:info).and_call_original
         msg = { job: 'IDVRedisToRedshiftJob',
                 success: true,
