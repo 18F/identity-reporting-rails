7bb690e4a54a127a530ef79fc94a5e7a49a44f25
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2025-09-15 10:30:27 -0700

    refactored to reflect latest architectural changes

--- app/jobs/attempts_api_import_job.rb
+++ app/jobs/attempts_api_import_job.rb
@@ -17,14 +17,14 @@ class AttemptsApiImportJob < ApplicationJob
     log_info('AttemptsApiImportJob: Job started', true)
 
     begin
-      response_data = fetch_api_data
+      response_data = fetch_redis_data
 
       log_info("AttemptsApiImportJob: Processing #{response_data[:sets].size} events", true)
 
       import_to_redshift(response_data[:sets])
 
       # todo: this is for testing purposes. Job will be handled in job_configurations.rb
-      # FcmsPiiDecryptJob.perform_now(PRIVATE_KEY.to_pem)
+      FcmsPiiDecryptJob.perform_now(PRIVATE_KEY.to_pem)
     rescue => e
       log_info('AttemptsApiImportJob: Error during API attempt', false, { error: e.message })
       raise
@@ -35,18 +35,20 @@ class AttemptsApiImportJob < ApplicationJob
 
   private
 
-  def fetch_api_data
-    sets = 999.times.map do |i|
+  def fetch_redis_data
+    999.times.map do |i|
       event_type = "event-#{SecureRandom.hex(4)}-#{i}"
-      encrypted_payload = encrypt_mock_jwt_payload(
-        mock_jwt_payload(event_type: event_type),
+      encrypted_payload = encrypt_mock_payload(
+        mock_payload(event_type: event_type),
         PUBLIC_KEY,
       )
 
-      { event_type => encrypted_payload }
-    end
-
-    { sets: sets }.with_indifferent_access
+      {
+        event_key: SecureRandom.uuid,
+        encrypted_data: encrypted_payload,
+        timestamp: Time.current,
+      }
+    end.with_indifferent_access
   end
 
   def import_to_redshift(encrypted_payloads)
@@ -58,7 +60,7 @@ class AttemptsApiImportJob < ApplicationJob
     end
 
     sql = <<~SQL
-      INSERT INTO fcms.unextracted_events (key_hash, message, event_timestamp)
+      INSERT INTO fcms.encrypted_events (event_key, message, event_timestamp)
       VALUES #{(['(?, ?, CURRENT_TIMESTAMP)'] * encrypted_payloads.size).join(', ')}
     SQL
 
@@ -90,7 +92,7 @@ class AttemptsApiImportJob < ApplicationJob
     )
   end
 
-  def encrypt_mock_jwt_payload(payload, public_key)
+  def encrypt_mock_payload(payload, public_key)
     jwk = JWT::JWK.new(public_key)
     JWE.encrypt(
       payload.to_json,
@@ -103,37 +105,31 @@ class AttemptsApiImportJob < ApplicationJob
     )
   end
 
-  def mock_jwt_payload(event_type:)
+  def mock_payload(event_type:)
     current_time = Time.current
 
     {
-      'jti' => SecureRandom.uuid,
-      'iat' => current_time.to_i,
-      'iss' => 'http://localhost:3000/',
-      'aud' => 'urn:gov:gsa:openidconnect:sp:sinatra',
-      'events' => {
-        "https://schemas.login.gov/secevent/attempts-api/event-type/#{event_type}" => {
-          'subject' => {
-            'subject_type' => 'session',
-            'session_id' => SecureRandom.uuid,
-          },
-          'occurred_at' => current_time.to_f,
-          'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36', # rubocop:disable Layout/LineLength
-          'unique_session_id' => SecureRandom.alphanumeric(20),
-          'user_uuid' => SecureRandom.uuid,
-          'device_id' => SecureRandom.hex(64),
-          'user_ip_address' => '::1',
-          'application_url' => 'http://localhost:9292/auth/result',
-          'language' => 'en',
-          'client_port' => nil,
-          'aws_region' => 'us-west-2',
-          'google_analytics_cookies' => {},
-          'mfa_device_type' => 'otp',
-          'reauthentication' => false,
-          'success' => true,
-          'agency_uuid' => SecureRandom.uuid,
-          'user_id' => rand(1..1000),
+      "https://schemas.login.gov/secevent/attempts-api/event-type/#{event_type}" => {
+        'subject' => {
+          'subject_type' => 'session',
+          'session_id' => SecureRandom.uuid,
         },
+        'occurred_at' => current_time.to_f,
+        'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36', # rubocop:disable Layout/LineLength
+        'unique_session_id' => SecureRandom.alphanumeric(20),
+        'user_uuid' => SecureRandom.uuid,
+        'device_id' => SecureRandom.hex(64),
+        'user_ip_address' => '::1',
+        'application_url' => 'http://localhost:9292/auth/result',
+        'language' => 'en',
+        'client_port' => nil,
+        'aws_region' => 'us-west-2',
+        'google_analytics_cookies' => {},
+        'mfa_device_type' => 'otp',
+        'reauthentication' => false,
+        'success' => true,
+        'agency_uuid' => SecureRandom.uuid,
+        'user_id' => rand(1..1000),
       },
     }.with_indifferent_access
   end
