2239f212845e1417f76cb99903f8bdf708f92a47
Sree Appachi <guruparan18@gmail.com>
2025-09-19 09:53:10 -0400

    Removed unused spec/test setup

--- spec/factories/fcms_encrypted_events.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :fcms_encrypted_event do
-    message { { text: Faker::Lorem.sentence } }
-    import_timestamp { Time.zone.now }
-    processsed_timestamp { Time.zone.now }
-  end
-end
--- spec/factories/fcms_events.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :fcms_event do
-    jti { SecureRandom.uuid }
-    message { Faker::Book.title }
-    import_timestamp { Time.zone.now }
-  end
-end
