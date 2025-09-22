ad03097b8242b79f212efcdf01e417846d513024
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2025-09-22 16:00:47 -0700

    Trying different implemetation to retrieve secret

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -61,12 +61,7 @@ class IdentityConfig
     config.add(:s3_report_bucket_prefix, type: :string)
     config.add(:redshift_sia_v3_enabled, type: :boolean)
     config.add(:fraud_ops_tracker_enabled, type: :boolean)
-
-    config.add(
-      :fraud_ops_private_key,
-      secrets_manager_name: "#{Identity::Hostdata.env || 'local'}/analytics/fraud-ops-private-key",
-      type: :string,
-    )
+    config.add(:fraud_ops_private_key, type: :string)
 
     "redshift/#{Identity::Hostdata.env || 'local'}-analytics-superuser".
       then do |redshift_secrets_manager_key|
