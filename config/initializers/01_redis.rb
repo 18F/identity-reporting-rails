9fe9bbcfce421a6cbdef750a24ba4d318e8bad75
Birane Seck <birane.seck@gsa.gov>
2025-10-06 13:28:27 -0400

    Feature flag Redis connection pool init
    (cherry picked from commit 64e86f02e7a019a1ab4f9bcb99895db51329563c)

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-REDIS_POOL = ConnectionPool.new(size: IdentityConfig.store.redis_pool_size) do
-  Redis.new(url: IdentityConfig.store.redis_url)
-end.freeze
+if IdentityConfig.store.fraud_ops_tracker_enabled
+  REDIS_POOL = ConnectionPool.new(size: IdentityConfig.store.redis_pool_size) do
+    Redis.new(url: IdentityConfig.store.redis_url)
+  end.freeze
+end
