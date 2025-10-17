d5755c51a962c65b23de44e2a699da5e789b4ef3
obinna.ebbis <obinna.ebbis@gsa.gov>
2025-10-17 18:51:19 +0000

    Update configs needed in reporting-rails

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -64,6 +64,7 @@ class IdentityConfig
     config.add(:redis_url, type: :string)
     config.add(:fraud_ops_tracker_enabled, type: :boolean)
     config.add(:fraud_ops_private_key, type: :string)
+    config.add(:aws_region, type: :string)
 
     "redshift/#{Identity::Hostdata.env || 'local'}-analytics-superuser".
       then do |redshift_secrets_manager_key|
