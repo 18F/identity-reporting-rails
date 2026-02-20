8cb5bbee61c1c62903cb19692d1542ff61c05a89
Aaron Nagucki <aaron.nagucki@gmail.com>
2026-02-20 15:10:23 -0600

    989/update reporting rails to version 8

--- config/initializers/ext_active_record_redshift_adapter.rb
+++ config/initializers/ext_active_record_redshift_adapter.rb
@@ -1,4 +1,4 @@
-if ActiveRecord.version >= Gem::Version.new('7.2.2')
+if ActiveRecord.version > Gem::Version.new('8.0.4')
   warn 'Unexpected ActiveRecord version, double check that the constructor ' \
        'monkeypatch is still needed'
 end
@@ -13,8 +13,45 @@ module IdentityReporting
     def create_database(name, positional_options = {}, **keyword_options)
       super(name, **positional_options.merge(keyword_options).transform_keys(&:to_sym))
     end
+
+    def create_schema(schema_name, *args, **kwargs)
+      super
+    rescue ActiveRecord::StatementInvalid => e
+      duplicate_schema_error = e.cause.is_a?(PG::DuplicateSchema) ||
+                               e.message.include?('PG::DuplicateSchema')
+
+      raise unless duplicate_schema_error
+    end
+  end
+
+  module RedshiftColumnMethodsOverride
+    def column(name, type, **options)
+      return super unless %i[json jsonb].include?(type&.to_sym)
+
+      super(name, 'SUPER', **options)
+    end
+
+    def primary_key(name, type = :primary_key, **options)
+      return super unless type == :uuid
+
+      options[:default] = options.fetch(:default, 'uuid_generate_v4()')
+      options[:primary_key] = true
+      column(name, type, **options)
+    end
+
+    def json(name, **options)
+      column(name, 'SUPER', **options)
+    end
+
+    def jsonb(name, **options)
+      column(name, 'SUPER', **options)
+    end
   end
 end
 
+puts "[Redshift Adapter] Registering 'redshift' adapter with ActiveRecord"
+
 ActiveRecord::ConnectionAdapters::Redshift::SchemaStatements.
   send(:prepend, IdentityReporting::SchemaStatementsOverride)
+ActiveRecord::ConnectionAdapters::Redshift::ColumnMethods.
+  send(:prepend, IdentityReporting::RedshiftColumnMethodsOverride)
