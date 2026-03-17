e6fa8948b8c3690732012daac1d13ba94783d43b
gerardo.cruz-ortiz <gerardo.cruz-ortiz@gsa.gov>
2026-03-17 17:52:46 -0400

    Refactor fallback implementation

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -10,7 +10,18 @@ cron_24h = '0 0 * * *'
 cron_24h_and_a_bit = '12 0 * * *' # 0000 UTC + 12 min, staggered from whatever else runs at 0000 UTC
 
 extractor_row_checker_enqueue_cron_config =
-  Fugit.parse_cron(IdentityConfig.store.extractor_row_checker_enqueue_cron).original
+  Fugit.parse_cron(IdentityConfig.store.extractor_row_checker_enqueue_cron).presence&.original
+
+# Malformed config will result in null value
+if extractor_row_checker_enqueue_cron_config.blank?
+  Rails.logger.warn(
+    "job_configurations: invalid extractor_row_checker_enqueue_cron=" \
+    "'#{IdentityConfig.store.extractor_row_checker_enqueue_cron}', " \
+    "defaulting to #{cron_1d}",
+  )
+  # Implement fallback
+  extractor_row_checker_enqueue_cron_config = cron_1d
+end
 
 if defined?(Rails::Console)
   Rails.logger.info 'job_configurations: console detected, skipping schedule'
