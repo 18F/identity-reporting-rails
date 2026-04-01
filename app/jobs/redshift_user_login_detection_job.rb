ffd47454d998d5148fe6e4ce00ce65aab0c8324c
samathad2023 <sdondeti@flexion.us>
2026-04-01 10:32:28 -0700

    update query to look for jdbc connections

--- app/jobs/redshift_user_login_detection_job.rb
+++ app/jobs/redshift_user_login_detection_job.rb
@@ -30,7 +30,8 @@ class RedshiftUserLoginDetectionJob < ApplicationJob
       FROM SYS_CONNECTION_LOG
       WHERE event = 'authenticated' 
       AND user_name IN (#{users_to_check.map { |s| "'#{s}'" }.join(", ")}) 
-      AND record_time >= CURRENT_TIMESTAMP - INTERVAL '15 MINUTES';
+      AND record_time >= CURRENT_TIMESTAMP - INTERVAL '15 MINUTES'
+      AND driver_version LIKE '%JDBC%';
     SQL
     result = DataWarehouseApplicationRecord.connection.execute(
       DataWarehouseApplicationRecord.sanitize_sql(query),
