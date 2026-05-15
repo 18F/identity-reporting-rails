f44c4e42f8fdbc93f6ad8cdede0884410c758cb2
ColtonGLapp <colton.lapp@gsa.gov>
2026-05-15 12:49:37 -0500

    update corn job scheduler for demographics 3rd of month

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -8,7 +8,7 @@ cron_1d = '0 6 * * *' # 6:00am UTC or 2:00am EST
 cron_1d_morning = '0 11 * * *' # 11:00am UTC or 6:00am EST
 cron_24h = '0 0 * * *'
 cron_24h_and_a_bit = '12 0 * * *' # 0000 UTC + 12 min, staggered from whatever else runs at 0000 UTC
-cron_monthly = '30 0 1 * *' # monthly, 0:30 UTC to not overlap with jobs running at 0000
+cron_3rd_of_month_6am = '0 6 3 * *' # 6 AM UTC on the 3rd of each month
 
 extractor_row_checker_enqueue_cron_config =
   Fugit.parse_cron(IdentityConfig.store.extractor_row_checker_enqueue_cron).presence&.original
@@ -75,8 +75,8 @@ else
       },
       demographics_metrics_report: {
         class: 'Reports::DemographicsMetricsReport',
-        cron: cron_monthly,
-        args: -> { [Time.zone.yesterday.end_of_day] },
+        cron: cron_3rd_of_month_6am,
+        args: -> { [Time.zone.now, 3, 'quarterly'] },
       },
       # Idv Legacy Conversion Supplement Report to S3
       idv_legacy_conversion_supplement_report: {
