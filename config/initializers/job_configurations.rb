e2396fdfcfd92943b75330fa065d43870d9bed86
samathad2023 <sdondeti@flexion.us>
2026-09-24 12:38:35 -0700

    job specs

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -2,9 +2,8 @@ require 'fugit'
 
 cron_hourly_offset = '55 * * * *'
 cron_15m = '*/15 * * * *'
-cron_fraud_ops_email_addresses_zero_etl = '*/15 * * * *'
-cron_quicksight_sync = '5/15 * * * *'
-cron_redshift_sync = '10/15 * * * *'
+cron_15m_5 = '5/15 * * * *'
+cron_15m_10 = '10/15 * * * *'
 cron_5m = '0/5 * * * *'
 # cron_10m = '0/10 * * * *'
 cron_1d = '0 6 * * *' # 6:00am UTC or 2:00am EST
@@ -137,12 +136,12 @@ else
       # Sync Redshift users - runs every 15 minutes
       redshift_sync_job: {
         class: 'RedshiftSyncJob',
-        cron: cron_redshift_sync,
+        cron: cron_15m_5,
       },
       # Sync QuickSight users - runs every 15 minutes
       quicksight_sync_job: {
         class: 'QuicksightSyncJob',
-        cron: cron_quicksight_sync,
+        cron: cron_15m_10,
       },
       # Refresh idp_core late-binding views from analytics_zetl db - runs daily,
       idp_zero_etl_sync_job: {
@@ -152,7 +151,7 @@ else
       # Sync fraudops email addresses from public.email_addresses in the zero-ETL database
       fraud_ops_email_addresses_zero_etl_job: {
         class: 'FraudOpsEmailAddressesZeroEtlJob',
-        cron: cron_fraud_ops_email_addresses_zero_etl,
+        cron: cron_15m,
       },
     }
     Rails.logger.info 'job_configurations: jobs scheduled with good_job.cron'
